b5bad1f0e79b9578f687062b1f31d6b26efba4bd
イ‐ヒョ‐ビン <113012132+hyobinaaa@users.noreply.github.com>
2023-03-15 20:49:44 +0900

    Add files via upload

---
+++ Variable.php
@@ -0,0 +1,16 @@
+<?php
+ // PHP 변수는 기호로 시작하고 $ 그 뒤에 변수 이름이 온다
+ 
+ $name = "hyo bin lee";
+ $age = 23;
+ $hobby = 'baseball';
+
+ // echo 는 출력문
+ echo $name;
+ echo "<br>";
+ echo $age;
+ echo "<br>";
+ echo $hobby;
+ 
+
+?>
